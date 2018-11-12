0c2ee590c7a8a4b80fc25457ac08f4a54813fe01
kunger47 <44657679+kunger47@users.noreply.github.com>
2018-11-12 13:22:53 -0500

    I added a grid of items, still needs lots of work

---
+++ CS360 Game/Assets/Scripts/equipment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class equipment : MonoBehaviour {
+
+    public Image img;
+    public bool inInventory;
+       
+	// Use this for initialization
+	void Start () {
+        inInventory = false;
+        img.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
