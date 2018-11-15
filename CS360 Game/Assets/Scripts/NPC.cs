c84f624c3bca2ba08a8d194a8878a5313ec39a23
Antonio Mendez <mendac01@pfw.edu>
2018-11-15 17:48:48 -0500

    adding GameInformation, NPC, and PartySlot scripts

---
+++ CS360 Game/Assets/Scripts/NPC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+	private int name;
+	string primaryName;
+	string secondaryName;
+	int primaryAttackBNS;
+	int primaryDefenseBNS;
+	int primaryHealthBNS;
+	int secondaryAttackBNS;
+	int secondaryDefenseBNS;
+	int secondaryHealthBNS;
+
+	public static string getNPCPrimaryName(int NPCID){
+		return GameInformation.NPCList[NPCID].primaryName;
+	}
+
+}
