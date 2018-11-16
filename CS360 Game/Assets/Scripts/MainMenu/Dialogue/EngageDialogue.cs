0a3854277a946a2ba4ce0bd2096c7ae6cb673956
Antonio Mendez <mendac01@pfw.edu>
2018-11-16 14:30:14 -0500

    adding EngageDialogue

---
+++ CS360 Game/Assets/Scripts/MainMenu/Dialogue/EngageDialogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class EngageDialogue : MonoBehaviour {
+
+	public int NPCID;
+	Button btn;
+	Text txt;
+  
+	public void Start() {
+		//GameObject.Find("TestButton").GetComponentInChildren<Text>().text = "something";
+		
+		btn = gameObject.GetComponent<Button>();
+		txt = btn.GetComponentInChildren<Text>();
+		txt.text = "something";
+		btn.onClick.AddListener(clicked);
+    }
+
+	public void clicked(){
+		txt.text = "something else";
+	}
+
+}
