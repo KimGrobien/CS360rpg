1da182bb3cbd89a8a9548ac729521aba5b3d45a9
Antonio Mendez <mendac01@pfw.edu>
2018-11-16 17:33:07 -0500

    doing stuff boi!!

--- CS360 Game/Assets/Scripts/MainMenu/Dialogue/EngageDialogue.cs
+++ CS360 Game/Assets/Scripts/MainMenu/Dialogue/EngageDialogue.cs
@@ -8,18 +8,24 @@ public class EngageDialogue : MonoBehaviour {
 	TextMeshProUGUI npcName;
 	Button choice1, choice2;
 	Text txt1, txt2;
+	Node[] currentDialogue;
   
 	public void Start() {
         npcName = GameObject.Find("NPC_Name").GetComponent<TextMeshProUGUI>();
-        npcName.text = "test"; //GameInfo.getName(GameInfo.currentNPC)
+        npcName.text = GameInfo.getName(GameInfo.currentNPC);//"test"; //GameInfo.getName(GameInfo.currentNPC)
 		
 		choice1 = GameObject.Find("Choice1").GetComponent<Button>();
         choice1.onClick.AddListener(clicked);
+		choice2 = GameObject.Find("Choice2").GetComponent<Button>();
+        choice2.onClick.AddListener(clicked);
 		txt1 = choice1.GetComponentInChildren<Text>();
+		txt2 = choice2.GetComponentInChildren<Text>();
+		currentDialogue = GameInfo.getDialogueTree(GameInfo.currentNPC);
+		txt1.text = currentDialogue[0].option1;
+		txt2.text = currentDialogue[0].option2;
     }
 
 	public void clicked(){
-		txt1.text = GameInfo.currentNPC.ToString();
 	}
 
 }
--- CS360 Game/Assets/Scripts/NPC.cs
+++ CS360 Game/Assets/Scripts/NPC.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC : MonoBehaviour {
+public class NPC{
 
 	public int name;
 	string primaryName;
