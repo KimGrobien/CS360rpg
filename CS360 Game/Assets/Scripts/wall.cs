9efaa2f66fb66264f552063e08e941e6ef9648c5
Kimberley <grobka01@students.ipfw.edu>
2018-11-17 10:01:58 -0500

    Added help and bounty boards

--- CS360 Game/Assets/Scripts/wall.cs
+++ CS360 Game/Assets/Scripts/wall.cs
@@ -3,33 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class wall : MonoBehaviour {
 	public bool interactable;
+	public string popupName;
 	public int id;
 	private bool touching;
+	private Canvas popup;
+	private Button cancel;
+
+	// Use this for initialization
+	void Start () {
+		if(popupName != ""){
+			popup = GameObject.Find(popupName).GetComponent<Canvas>();
+			cancel = popup.GetComponentInChildren<Button>();
+			cancel.onClick.AddListener(closePopup);
+			Debug.Log("ADDED");
+		}
+	}
 	void OnCollisionEnter2D(Collision2D other){
 		touching = true;
 		if(interactable){
 			GameInfo.currentNPC = id;
 		}
+		if(popupName != ""){
+			popup.enabled = true;
+		}
 	}
 	void OnCollisionExit2D(){
 		touching = false;
 	}
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(touching){
+		if(touching && interactable){
 			if(Input.GetKeyDown(KeyCode.I)){
 				GameInfo.prevScene = SceneManager.GetActiveScene().buildIndex;
 				SceneManager.LoadScene("Menu");
 			}
 		}
+	}
+
+	private void closePopup(){
+		Debug.Log("REACHED");
+		popup.enabled = false;
 	}
 }
