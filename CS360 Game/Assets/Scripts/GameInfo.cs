1eb447b9b76cb63eb18f74563728e4ebdf96bc8a
Antonio Mendez <mendac01@pfw.edu>
2018-11-18 03:01:08 -0500

    updating the engageDialogue and made some changes in the menu ui and commented some code for readability

--- CS360 Game/Assets/Scripts/GameInfo.cs
+++ CS360 Game/Assets/Scripts/GameInfo.cs
@@ -46,7 +46,11 @@ public class GameInfo : MonoBehaviour
     private static int health = 100;
 
     // Name, Move One, Move Two
-    private static string[,] NPCstringData = new string[11, 3] { { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" } };
+    /*NPC ID will be array Index 
+    (RECRUITABLES) Cynthia = 0, Anker(ShopKeeper) = 1, Edward(Doctor) = 2, Emrick(Farmer) = 3, 
+    (SHADOWS)      Berndy(Bunny) = 4, Modir(Mother) = 5, Farenvir(Father) = 6, Ozul(Antagonist) = 7,
+    (BOUNTY)       Fox = 8, Rock Creature = 9, Rabbit = 10*/
+    private static string[,] NPCstringData = new string[11, 3] { { "Cynthia", "Heal", "Revive" }, { "Anker", "Use Item", "Rage" }, { "Edward", "Heal", "Revive" }, {"Emrik", "Impale", "Kick" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" }, { "Cynthia", "Heal", "Revive" } };
     // Heath, PrimaryStat, SecondaryStat, RunRange (FOR first 4 (0 to 3 ID indexes) Recruitable NPCs)
     // Heath, Set Attack, additional Attack Range to be added to Attack, RunRange (last 7(6 to 10 ID indexes) enemy NPCs)
     private static int[,] NPCintData = new int[11, 4] { { 25, 50, 0, 1 }, { 150, 150, 35, 10 }, { 75, 25, 30, 6 }, { 35, 50, 20, 8 }, { 65, 15, 5, 5 }, { 100, 35, 10, 10 }, { 120, 25, 7, 7 }, { 250, 75, 10, 100 }, { 25, 5, 2, 3 }, { 50, 10, 10, 5 }, { 25, 3, 2, 1 } };
@@ -61,35 +65,12 @@ public class GameInfo : MonoBehaviour
     public static Node[] Anker = new Node[10];
     public static Node[] Edward = new Node[10];
 
+
     
 
     private void Start()
     {
-        Cynthia[0].response = "Pick an option";
-        Cynthia[0].option1 = "This is a new Option, do you want to pick it?";
-        Cynthia[0].option2 = "Way to go!";
-        Cynthia[0].indexForOption1 = 1;
-        Cynthia[0].indexForOption2 = 2;
-
-        Cynthia[1].response = "Woah OPTIONS?! well pick 1 or 2.";
-        Cynthia[1].option1 = "this one";
-        Cynthia[1].option2 = "this two";
-        Cynthia[1].indexForOption1 = 3;
-        Cynthia[1].indexForOption2 = 4;
-
-        Cynthia[2].response = "You picked the new option, huh?";
-        Cynthia[2].option1 = "Now what?";
-        Cynthia[2].option2 = "Yes, yes I did";
-        Cynthia[2].indexForOption1 = 5;
-        Cynthia[2].indexForOption2 = 6;
-           
-        Cynthia[3].response = "So you picked this one";
-        Cynthia[3].option1 = "yes I did brother bear";
-        Cynthia[3].option2 = "I didn't mean to";
-        Cynthia[3].indexForOption1 = 7;
-        Cynthia[3].indexForOption2 = 8;
-
-        DialogueTrees[0] = Cynthia;
+        
         DontDestroyOnLoad(this.gameObject);
 
         int i = 0;
@@ -104,6 +85,8 @@ public class GameInfo : MonoBehaviour
                 //populate npcintdata
             }
         }
+        LoadDialogueTrees();
+        
     }
     public static string getName(int idx){
         //return NPCList[idx].name;
@@ -112,5 +95,58 @@ public class GameInfo : MonoBehaviour
     public static Node[] getDialogueTree(int index){
         return DialogueTrees[index];
     }
-    //return DialogueTrees[idx]
+    public static Sprite GetImageOfCurrentNPC(int index){
+        //string characterName = getName(index);
+        //return Resources.Load<Sprite>("Art/DialogueImages/"+characterName);
+        return Resources.Load<Sprite>("Art/DialogueImages/Ego");
+    }    
+
+    private static void LoadDialogueTrees(){
+        //Cynthia
+        //currentIndex will lead to currentIndex+3 as option1 and currentIndex+4 as option2
+       //firstEncounter
+        Cynthia[0].response = "You approach a small woman standing next to a garden." 
+        +" Her hands are covered in dirt and her forehead in sweat. You can tell she's" 
+        +" been working outside all day. She notices you walking up to her. She gives"
+        +" you a kind smile. \n\nShe says, \"You\'re new, aren\'t you? I haven\'t seen you around here at least. Is there something you need traveler?\"";
+        Cynthia[0].option1 = "I'm hurt can you help me?";
+        Cynthia[0].option2 = "Where is the nearest town?";
+        Cynthia[0].indexForOption1 = 1;
+        Cynthia[0].indexForOption2 = 2;
+        //Response to index 0 option1
+        Cynthia[1].response = "She looks concerned, but hesitant to help."
+        +" You think maybe she's just being defensive, a good trait to have"
+        +" in this world, but you are really hurt."
+        +"\n\nShe says, \"What happened? Were you traveling alone?\"";
+        Cynthia[1].option1 = "I don't remember what happened."
+        +" I woke up next to a dismembered horse and destroyed carraige."
+        +" I think someone robbed me. If you can\'t help me. "
+        +"Do you know someone who can?";
+        Cynthia[1].option2 = "this two";
+        Cynthia[1].indexForOption1 = 3;
+        Cynthia[1].indexForOption2 = 4;
+        //response to index 0 option2
+        Cynthia[2].response = "She says, \"You can find a small village just up the road."
+        +"Though they may not be much help to you.\"";
+        Cynthia[2].option1 = "Why is that?";
+        Cynthia[2].option2 = "As long as they have a doctor then I don't care!";
+        Cynthia[2].indexForOption1 = 5;
+        Cynthia[2].indexForOption2 = 6;
+        //response to index 1 option1
+        Cynthia[3].response = "Your story seems to have struck a chord with her."
+        +" Her guarded dimeanor seems to soften. "
+        +"She says, \"I think I can help. Hold on.\"\nShe raises her hands in your direction. "
+        +"A bright light emits from her palms. You feel a warmth in your chest and start feeling a little better.";
+        Cynthia[3].option1 = "That was amazing! Thank you so much for helping me. "
+        +"I don't know a lot of strangers that would be willing to help.";
+        Cynthia[3].option2 = "";
+        Cynthia[3].indexForOption1 = 7;
+        Cynthia[3].indexForOption2 = 8;
+
+        DialogueTrees[0] = Cynthia;
+    }
+
+    public static void UpdateHealth(int heal){
+        health+=heal;
+    }
 }
--- CS360 Game/Assets/Scripts/MainMenu/Dialogue/EngageDialogue.cs
+++ CS360 Game/Assets/Scripts/MainMenu/Dialogue/EngageDialogue.cs
@@ -12,6 +12,8 @@ public class EngageDialogue : MonoBehaviour {
 	Node[] currentDialogue;
 	int indexForNextOption1;
 	int indexForNextOption2;
+	Sprite npcImage;
+	GameObject imagePlace;
   
 	public void Start() {
 		//Get cancel button
@@ -19,16 +21,18 @@ public class EngageDialogue : MonoBehaviour {
 		cancel.onClick.AddListener(cancelMenu);
 		//Get NPCName Text Data
         npcName = GameObject.Find("NPC_Name").GetComponent<TextMeshProUGUI>();
+		//GetNPCImage Data
+		//update the image to current npc engaging with THIS DOESN"T WORK CURRENTLY DON"T KNOW WHY
+		npcImage = GameInfo.GetImageOfCurrentNPC(GameInfo.currentNPC);
+		GameObject.Find("NPC_Image").GetComponent<Image>().sprite = npcImage;
 		//Update the name of the npc to current npc engaging with
         npcName.text = GameInfo.getName(GameInfo.currentNPC);//"test"; //GameInfo.getName(GameInfo.currentNPC)
 		//get Response game object
 		npcResponse = GameObject.Find("Response").GetComponent<TextMeshProUGUI>();
 		//get choice button game objects
 		choice1 = GameObject.Find("Choice1").GetComponent<Button>();
-        choice1.onClick.AddListener(()=>clickedOption1(indexForNextOption1));
 		choice2 = GameObject.Find("Choice2").GetComponent<Button>();
-        choice2.onClick.AddListener(()=>clickedOption2(indexForNextOption2));
-		//get the text for these objects
+		//get the text for these objects	
 		txt1 = choice1.GetComponentInChildren<Text>();
 		txt2 = choice2.GetComponentInChildren<Text>();
 		//find the tree that needs to be traversed
@@ -37,14 +41,35 @@ public class EngageDialogue : MonoBehaviour {
 		npcResponse.text = currentDialogue[0].response;
 		txt1.text = currentDialogue[0].option1;
 		txt2.text = currentDialogue[0].option2;
+		
+		indexForNextOption1 = currentDialogue[0].indexForOption1;
+		indexForNextOption2 = currentDialogue[0].indexForOption2;
+        choice1.onClick.AddListener(()=>clickedOption1(indexForNextOption1));
+        choice2.onClick.AddListener(()=>clickedOption2(indexForNextOption2));
     }
 	public void clickedOption1(int index){
+		Debug.Log(index);
+		if(index==0){
+			index++;
+		
 		npcResponse.text = currentDialogue[index].response;
 		txt1.text = currentDialogue[index].option1;
 		txt2.text = currentDialogue[index].option2;
 		indexForNextOption1 = currentDialogue[index].indexForOption1;
 		indexForNextOption2 = currentDialogue[index].indexForOption2;
+		index--;
 
+		}
+		if(index==3&&npcName.text=="Cynthia"){
+			//heal Ego
+			GameInfo.UpdateHealth(50);
+
+		}
+		npcResponse.text = currentDialogue[index].response;
+		txt1.text = currentDialogue[index].option1;
+		txt2.text = currentDialogue[index].option2;
+		indexForNextOption1 = currentDialogue[index].indexForOption1;
+		indexForNextOption2 = currentDialogue[index].indexForOption2;
 	}
 	public void clickedOption2(int index){
 		npcResponse.text = currentDialogue[index].response;
