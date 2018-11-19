ebc39bbcf4176d4f5f4bd3b3c234c8bb46fcca08
kunger47 <44657679+kunger47@users.noreply.github.com>
2018-11-19 09:28:36 -0500

    money system for purchasing fixed

--- CS360 Game/Assets/Scripts/MainMenu/InventoryController.cs
+++ CS360 Game/Assets/Scripts/MainMenu/InventoryController.cs
@@ -5,15 +5,17 @@ using TMPro;
 using UnityEngine.UI;
 
 public class InventoryController : MonoBehaviour {
-    private TextMeshProUGUI equipmentName;
+    private TextMeshProUGUI equipmentName, EgosMoney;
     private int currentItem;
+    private Color TextColor;
     private Button[] equip = new Button[15];
     private Image EgosPrimary,EgosSecondary,EgosDefense;
     private Button primaryButton, secondaryButton, defenseButton, buyButton, enableBuying;
     private equipmentData buying;
-    Text EgosMoney;
-    // Use this for initialization
+
+    // Used for initialization of menu
     void Start () {
+
         for (int i = 0; i < 15; i++)
         {
             equip[i] = GameObject.Find("slot" + i).GetComponent<Button>();
@@ -41,8 +43,9 @@ public class InventoryController : MonoBehaviour {
         buyButton.interactable = false;
         defenseButton.interactable = false;
 
-        EgosMoney = GameObject.Find("MoneyCount").GetComponent<Text>();
-        //EgosMoney.text = "" + GameInfo.getMoney();
+        EgosMoney = GameObject.Find("MoneyCount").GetComponentInChildren<TextMeshProUGUI>();
+        TextColor = EgosMoney.color;
+        EgosMoney.text = "" + GameInfo.getMoney();
     }
 	
 	// Update is called once per frame
@@ -55,7 +58,7 @@ public class InventoryController : MonoBehaviour {
     // Make sure to pass id of eaah item
     private void ItemClicked(int i)
     {
-        
+        // For each object during buying mode (shop items only)
         for (int j = 0; j < 12; j++)
         {
             if (!(j == 3 || j == 7 || j == 11))
@@ -67,9 +70,8 @@ public class InventoryController : MonoBehaviour {
                 }
             }
         }
-        //Image equipImg;
-        //equipImg = GameObject.Find("slot" + (i)).GetComponent<Image>();
-        //equipImg.enabled = !equipImg.enabled;
+        
+        //You own the object or in buying mode
         if (GameInfo.getEquipment(i).owned || GameInfo.buyingMode)
         {
             currentItem = i;
@@ -79,46 +81,58 @@ public class InventoryController : MonoBehaviour {
             equipmentName.text = GameInfo.getEquipment(i).description;
         }
 
-        //Check if equipment is attack or defense or buying
-        if (!GameInfo.getEquipment(i).owned && GameInfo.buyingMode && GameInfo.getEquipment(i).Price < GameInfo.getMoney())
+        //Buying Mood and you have enough Money
+        if (!GameInfo.getEquipment(i).owned && GameInfo.buyingMode && GameInfo.getEquipment(i).Price <= GameInfo.getMoney())
         {
-            primaryButton.interactable = false;
-            secondaryButton.interactable = false;
-            buyButton.interactable = true;
-            defenseButton.interactable = false;
+            SetButtonsVisablity(false, false, false, true);
+            EgosMoney.color = Color.green;
             equip[i].image.color = Color.red;
-        } else if(GameInfo.getEquipment(i).owned && GameInfo.buyingMode) {
-            primaryButton.interactable = false;
-            secondaryButton.interactable = false;
-            buyButton.interactable = false;
-            defenseButton.interactable = false;
         }
-        else if (GameInfo.getEquipment(i).owned && !GameInfo.buyingMode){
-            //if attack
+        // Dont have enough money
+        else if (!GameInfo.getEquipment(i).owned && GameInfo.buyingMode && GameInfo.getEquipment(i).Price > GameInfo.getMoney())
+        {
+            SetButtonsVisablity(false, false, false, false);
+            EgosMoney.color = Color.red;
+            equip[i].image.color = Color.red;
+        }
+        // Buying mode but you already own the object
+        else if (GameInfo.getEquipment(i).owned && GameInfo.buyingMode)
+        {
+            SetButtonsVisablity(false, false, false, false);
+            EgosMoney.color = TextColor;
+        }
+        // you own the object and its not in buying mode
+        else if (GameInfo.getEquipment(i).owned && !GameInfo.buyingMode)
+        {
+            EgosMoney.color = TextColor;
+            //if attack object
             if (i >= 0 && i < 8 || i == 11)
             {
-                primaryButton.interactable = true;
-                secondaryButton.interactable = true;
-                buyButton.interactable = false;
-                defenseButton.interactable = false;
+                SetButtonsVisablity(true, true, false, false);
             }
+            // if defense object
             else if (i > 7 && i < 11)
             {
-                primaryButton.interactable = false;
-                secondaryButton.interactable = false;
-                buyButton.interactable = false;
-                defenseButton.interactable = true;
+                SetButtonsVisablity(false, false, true, false);
             }
+            //if nothing
             else
             {
-                primaryButton.interactable = false;
-                secondaryButton.interactable = false;
-                buyButton.interactable = false;
-                defenseButton.interactable = false;
+                SetButtonsVisablity(false, false, false, false);
             }
         }
     }
 
+    // Sets Visablity of the four option buttons for equipment
+    private void SetButtonsVisablity(bool pri, bool sec, bool def, bool buy)
+    {
+        primaryButton.interactable = pri;
+        secondaryButton.interactable = sec;
+        buyButton.interactable = def;
+        defenseButton.interactable = buy;
+    }
+
+    // Set time to primary combat move
     private void PrimaryButtonClicked(int i)
     {
         EgosPrimary = GameObject.Find("EquipSlot0").GetComponent<Image>();
@@ -127,6 +141,7 @@ public class InventoryController : MonoBehaviour {
         GameInfo.UpdateEgosPrimary(GameInfo.getEquipment(i));
     }
 
+    // Set item to secondary combat move
     private void SecondaryButtonClicked(int i)
     {
         EgosSecondary = GameObject.Find("EquipSlot1").GetComponent<Image>();
@@ -135,6 +150,7 @@ public class InventoryController : MonoBehaviour {
         GameInfo.UpdateEgosSecondary(GameInfo.getEquipment(i));
     }
 
+    // Set item to defense button 
     private void DefenseButtonClicked(int i)
     {
         EgosDefense = GameObject.Find("EquipSlot2").GetComponent<Image>();
@@ -143,16 +159,19 @@ public class InventoryController : MonoBehaviour {
         GameInfo.UpdateEgosDefense(GameInfo.getEquipment(i));
     }
 
+    // Buy item button actions
     private void BuyButtonClicked(int i)
     {
         GameInfo.setEquipmentColor(i, Color.white);
         GameInfo.setEquipmentOwned(i);
         equip[i].image.color = GameInfo.getEquipment(i).Visability;
         GameInfo.reduceMoney(GameInfo.getEquipment(i).Price);
-        //EgosMoney.text = "" + GameInfo.getMoney();
+        EgosMoney.text = "" + GameInfo.getMoney();
         buyButton.interactable = false;
+        EgosMoney.color = TextColor;
     }
 
+    // For Testing Purposes
     private void toggleBuyingMode()
     {
         GameInfo.buyingMode = !GameInfo.buyingMode;
