5285a2dfa622d3d5c435a15253a9679099ad2e23
Kimberley <grobka01@students.ipfw.edu>
2018-11-24 20:47:05 -0500

    Added location tracking

--- CS360 Game/Assets/Scripts/PlayerController.cs
+++ CS360 Game/Assets/Scripts/PlayerController.cs
@@ -9,38 +9,39 @@ public class PlayerController : MonoBehaviour {
 	public float moveSpeed;
 	private Animator anim;
 	// Use this for initialization
-	private bool destroyed = false; //Needed since OnDestroy is getting called multiple times due to Unity bug
 	void Start () {
         moveSpeed = 3;
-		if(GameInfo.currentNPC >= 0){//If interacting with NPC, set position based on before menu scene
+		if (GameInfo.prevPos.x != -999){
 			this.transform.position = GameInfo.prevPos;
-			GameInfo.currentNPC = -1;
-		}else{
-			if((SceneManager.GetActiveScene().name == "Castle") && (GameInfo.prevScene == 6)){
-			this.transform.position = new Vector3(-.25f, -.43f, 0);//Entering castle scene from inside castle
-			}
-			if (SceneManager.GetActiveScene().name == "Town"){
-				//Town has several entry points so we must set Ego's position in the town scene based on where he entered 
-				switch (GameInfo.prevScene)
-				{
-					case 0://Leaving title screen, initial start of game
-						this.transform.position = new Vector3(-1.57f, 1.06f, 0);//Set position to path leaving castle
-						break;
-					case 3://Entering town from castle
-						this.transform.position = new Vector3(-8.07f, -4.72f, 0);//Set position to path leaving castle
-						break;
-					case 4://Entering town from bounty area
-						this.transform.position = new Vector3(.36f, 6.84f, 0);//Set position to path leaving bounty
-						break;
-					case 7: //Entering town from hospital
-						this.transform.position = new Vector3(-4.43f, -.79f, 0);//Set position to path leaving hospital
-						break;
-					case 8: //Entering town from shop
-						this.transform.position = new Vector3(6.17f,5.32f, 0);//Set position to path leaving shop
-						break;
-				}
-			}
 		}
+		if(GameInfo.currentNPC >= 0){
+			GameInfo.currentNPC = -1; //No longer interacting with NPC
+		}
+		// 	if((SceneManager.GetActiveScene().name == "Castle") && (GameInfo.prevScene == 6)){
+		// 	//this.transform.position = new Vector3(-.25f, -.43f, 0);//Entering castle scene from inside castle
+		// 	}
+		// 	if (SceneManager.GetActiveScene().name == "Town"){
+		// 		//Town has several entry points so we must set Ego's position in the town scene based on where he entered 
+		// 		/*switch (GameInfo.prevScene)
+		// 		{
+		// 			case 0://Leaving title screen, initial start of game
+		// 				this.transform.position = new Vector3(-1.57f, 1.06f, 0);//Set position to path leaving castle
+		// 				break;
+		// 			case 3://Entering town from castle
+		// 				this.transform.position = new Vector3(-8.07f, -4.72f, 0);//Set position to path leaving castle
+		// 				break;
+		// 			case 4://Entering town from bounty area
+		// 				this.transform.position = new Vector3(.36f, 6.84f, 0);//Set position to path leaving bounty
+		// 				break;
+		// 			case 7: //Entering town from hospital
+		// 				this.transform.position = new Vector3(-4.43f, -.79f, 0);//Set position to path leaving hospital
+		// 				break;
+		// 			case 8: //Entering town from shop
+		// 				this.transform.position = new Vector3(6.17f,5.32f, 0);//Set position to path leaving shop
+		// 				break;
+		// 		}
+		// 	}
+		// }
 		anim = GetComponent<Animator>();
 	}
 	
--- CS360 Game/Assets/Scripts/enterNewArea.cs
+++ CS360 Game/Assets/Scripts/enterNewArea.cs
@@ -6,21 +6,41 @@ using UnityEngine.SceneManagement;
 public class enterNewArea : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other) {
             GameInfo.prevScene = SceneManager.GetActiveScene().buildIndex; 
+            GameInfo.prevPos.x = -999;//-999 special value to signify ignore prevPos
             switch (this.name)
             {
                 case "enter_start":
                     SceneManager.LoadScene(1);
                     break;
                 case "enter_town":
+                    //Account for 4 alternate entry points to town
+                    switch (SceneManager.GetActiveScene().name){
+                        case "Castle":
+                            GameInfo.prevPos = new Vector3(-8.07f, -4.72f, 0);
+                            break;
+                        case "Bounty":
+                           GameInfo.prevPos = new Vector3(.36f, 6.84f, 0);
+                            break;
+                        case "Shop":
+                            GameInfo.prevPos = new Vector3(-5.68f, -4.49f, 0);
+                            break;
+                        case "Hospital":
+                            GameInfo.prevPos = new Vector3(-4.43f, -.79f, 0);
+                            break;
+                    }
                     SceneManager.LoadScene(2);
-                    break;
-                case "enter_castle":
-                    SceneManager.LoadScene(3);
                     break;
                 case "enter_boss_room":
                     SceneManager.LoadScene("Boss Room");
                     break;
+                case "enter_castle_area":
+                    if(SceneManager.GetActiveScene().name == "Castle Hall"){
+                        GameInfo.prevPos = new Vector3(-.25f, -.43f, 0);
+                    }
+                    SceneManager.LoadScene("Castle");
+                    break;
                 case "enter_bounty":
+                    GameInfo.prevPos.y *= -1;
                     SceneManager.LoadScene(4);
                     break;
                 case "enter_hospital":
@@ -33,9 +53,11 @@ public class enterNewArea : MonoBehaviour {
                     SceneManager.LoadScene("Castle Hall");
                     break;
                 case "exit_castle":
+                    GameInfo.prevPos = new Vector3(-.25f, -.43f, 0);
                     SceneManager.LoadScene("Castle");
                     break;
                 case "exit_boss_room":
+                    GameInfo.prevPos = new Vector3(-.3f, 3.02f, 0);
                     SceneManager.LoadScene("Castle Hall");
                     break;
                 case "exit_hospital":
