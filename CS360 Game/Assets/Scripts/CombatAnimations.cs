4778576d95ea0fa59591d1aaf28220fde9c17c79
Kimberley <grobka01@students.ipfw.edu>
2018-12-02 11:26:29 -0500

    Added combat animations

---
+++ CS360 Game/Assets/Scripts/CombatAnimations.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatAnimations : MonoBehaviour {
+
+	private Animator anim;
+	void Start () {
+		anim = GameObject.Find("Enemy").GetComponent<Animator>();
+		Debug.Log(anim);
+		anim.SetInteger("id", GameInfo.currentNPC);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
