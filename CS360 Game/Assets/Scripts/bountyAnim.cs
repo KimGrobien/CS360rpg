c9fb1d97b21eaf2de3635e8aa0f90999d769cfd3
Antonio Mendez <mendac01@pfw.edu>
2018-12-07 17:13:59 -0500

    fixed issue where if holding more than 1 bounty upon redemption all bounties would be removed from inventory

--- CS360 Game/Assets/Scripts/bountyAnim.cs
+++ CS360 Game/Assets/Scripts/bountyAnim.cs
@@ -23,27 +23,34 @@ public class bountyAnim : MonoBehaviour {
 
     private void Update(){
     
-        if (GameInfo.getEquipment(idx).owned && Input.GetKeyDown(KeyCode.S))
-        {
-            if (idx == 14)
+        if(inTrigger){
+            if (GameInfo.getEquipment(idx).owned && Input.GetKeyDown(KeyCode.S))
             {
-                GameInfo.updateNPCHealth(8, -GameInfo.getNPCMAXHealth(8));
-                GameInfo.setNotDead(8);
+                if (idx == 14&&nameOfShowMoney=="show_money_0")
+                {
+                    GameInfo.updateNPCHealth(8, -GameInfo.getNPCMAXHealth(8));
+                    GameInfo.setNotDead(8);
+                    GameInfo.AddMoney(GameInfo.GetPrice(idx));
+                    GameInfo.setBountyNotOwned(idx);
+                }
+                else if (idx == 12&&nameOfShowMoney=="show_money_1")
+                {
+                    GameInfo.updateNPCHealth(9, -GameInfo.getNPCMAXHealth(9));
+                    GameInfo.setNotDead(9);
+                    GameInfo.AddMoney(GameInfo.GetPrice(idx));
+                    GameInfo.setBountyNotOwned(idx);
+                }
+                else
+                {
+                    GameInfo.updateNPCHealth(10, -GameInfo.getNPCMAXHealth(10));
+                    GameInfo.setNotDead(10);
+                    GameInfo.AddMoney(GameInfo.GetPrice(idx));
+                    GameInfo.setBountyNotOwned(idx);
+                }
+                
+                showMoney.enabled = false;
+                interactAnim.enabled=false;
             }
-            else if (idx == 12)
-            {
-                GameInfo.updateNPCHealth(9, -GameInfo.getNPCMAXHealth(9));
-                GameInfo.setNotDead(9);
-            }
-            else
-            {
-                GameInfo.updateNPCHealth(10, -GameInfo.getNPCMAXHealth(10));
-                GameInfo.setNotDead(10);
-            }
-            GameInfo.AddMoney(GameInfo.GetPrice(idx));
-            GameInfo.setBountyNotOwned(idx);
-            showMoney.enabled = false;
-            interactAnim.enabled=false;
         }
     }
 
