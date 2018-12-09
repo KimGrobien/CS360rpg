6adaa5e003160ea7a7e1c3bb5228f7a20fe83504
Antonio Mendez <mendac01@pfw.edu>
2018-12-08 23:24:10 -0500

    more comments

--- CS360 Game/Assets/Scripts/MainMenu/QuitGame.cs
+++ CS360 Game/Assets/Scripts/MainMenu/QuitGame.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 public class QuitGame : MonoBehaviour {
 
+//The name of the game object within the scene
 public string buttonName;
+//This is a reference to the game object within the scene.
 Button quitButton;
+
+/// <summary>
+///This function is responsible for instantiating the button and the listener for the button
+/// <summary>
 void Start(){
 	quitButton = GameObject.Find(buttonName).GetComponent<Button>();
 	quitButton.onClick.AddListener(CloseGame);
 }
 
+/// <summary>
+/// Closes the application
+/// <summary>
 void CloseGame(){
 		Application.Quit();
 }
